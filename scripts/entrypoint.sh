1650e2881bbc91b0b90ac684597cecbff6783f48
Raúl Kuzner <raul@kuzner.net>
2024-09-15 18:05:33 -0300

    allow sim command and crontab schedule via env vars

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $# -eq 1 ]
-  then
-    (crontab -l 2>/dev/null; echo "$1 /usr/local/bin/organize run >> /var/log/organize/organize.log 2>&1") | crontab -
+# exit immediately on any shell error
+set -e
+
+# set the default ORGANIZE_COMMAND
+THE_ORGANIZE_COMMAND="run"
+
+# evaluate if ORGANIZE_COMMAND was set on ENV, if so, only use if valid
+if [ -n "${ORGANIZE_COMMAND}" ]; then
+  if [ "${ORGANIZE_COMMAND}" = "run" ] || [ "${ORGANIZE_COMMAND}" = "sim" ]; then
+    THE_ORGANIZE_COMMAND="${ORGANIZE_COMMAND}"
+  fi
+fi
+
+# check if ORGANIZE_SCHEDULE was set on ENV. if so, set crontab schedule with it; and keep the image running...
+if [ -n "${ORGANIZE_SCHEDULE}" ]; then
+    (crontab -l 2>/dev/null; echo "${ORGANIZE_SCHEDULE} /usr/local/bin/organize ${THE_ORGANIZE_COMMAND} >> /var/log/organize/organize.log 2>&1") | crontab -
     service cron restart
     /bin/bash
-  else
-	  /usr/local/bin/organize run >> /var/log/organize/organize.log 2>&1 && exit 0
+fi
+
+# at this point, only a single run should occur
+if [ -z "${ORGANIZE_SCHEDULE}" ]; then
+  organize "${THE_ORGANIZE_COMMAND}" >> /var/log/organize/organize.log 2>&1
 fi
