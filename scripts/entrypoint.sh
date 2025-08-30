c733c3d7752a599e12406eecfc555eb8e3a3cacb
Raúl Kuzner <raul@kuzner.net>
2025-08-30 19:47:41 -0300

    entrypoint script optimization

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,73 +1,145 @@
 #!/bin/bash
 
-# exit immediately on any shell error
-#set -e
-
+# import log_message function
+scriptFolder=$( cd "$( dirname "${0}" )" && pwd )
 scriptBaseName=$( basename "${0}" .sh )
-logFolder="/logs"
-logFile="${logFolder}/${scriptBaseName}-$( date +%F ).log"
-
-# logs a message to console AND to logfile
-function log_message() {
-	local message2log="${*}"
-	if [ -n "${message2log}" ]; then
-		timeStamp=$( date "+%Y/%m/%d %H:%M:%S,%3N" )		# ej: 2018/02/02 15:34:02,241
-		echo "${timeStamp} | ${message2log}" | tee -a "${logFile}"
-	fi
-}
-
-# set the default ORGANIZE_COMMAND
-THE_ORGANIZE_COMMAND="run"
-
-log_message " -+*+- -+*+- -+*+- -+*+- "
-log_message "checking path for Organize Config file..."
-whatPath=$(organize show --path)
-log_message "...found: ${whatPath}"
-
-log_message "Checking whether the config file has valid contents..."
-checkResult=$(organize check)
-log_message "...result: ${checkResult}"
-
-log_message "Preparing organize-run.conf file..."
-echo "#!/bin/bash" > /home/ot/organize-run.conf
-log_message "Append ORGANIZE_CONFIG to organize-run.conf file"
-echo 'THE_ORGANIZE_CONFIG="'${ORGANIZE_CONFIG}'"' >> /home/ot/organize-run.conf
-
-# evaluate if ORGANIZE_COMMAND was set on ENV, if so, only use if valid
-if [ -n "${ORGANIZE_COMMAND}" ]; then
-  log_message "Found ORGANIZE_COMMAND environment var!"
-  if [ "${ORGANIZE_COMMAND}" = "run" ] || [ "${ORGANIZE_COMMAND}" = "sim" ]; then
-    THE_ORGANIZE_COMMAND="${ORGANIZE_COMMAND}"
-    log_message "Valid ORGANIZE_COMMAND environment var: ${ORGANIZE_COMMAND}"
-  fi
+if [ -n "${TOOL_NAME}" ]; then
+	scriptBaseName="${scriptBaseName}-"$( echo ${TOOL_NAME} | tr "[:upper:]" "[:lower:]" )
 fi
-log_message "Using THE_ORGANIZE_COMMAND: ${THE_ORGANIZE_COMMAND}"
+source "${scriptFolder}/log-message.sh"
 
-log_message "Append ORGANIZE_COMMAND to organize-run.conf file"
-echo 'THE_ORGANIZE_COMMAND="'${THE_ORGANIZE_COMMAND}'"' >> /home/ot/organize-run.conf
+toolBaseFileName="tool-run"
+toolScriptFileName="${toolBaseFileName}.sh"
+toolConfigFileName="${toolBaseFileName}.conf"
+userName="theuser"
+userHomeFolder="home/${userName}"
 
-log_message "Done preparing organize-run.conf file."
+DEFAULT_LOG_FOLDER="/logs"
+DEFAULT_SOURCE_FOLDER="/source"
+DEFAULT_TARGET_FOLDER="/target"
+DEFAULT_DATA_FOLDER="/data"
+DEFAULT_ORGANIZE_CONFIG="/config/config.yaml"
+DEFAULT_ORGANIZE_COMMAND="run"
 
-# check if ORGANIZE_SCHEDULE was set on ENV. if so, set crontab schedule with it
-if [ -n "${ORGANIZE_SCHEDULE}" ]; then
-  log_message "Found ORGANIZE_SCHEDULE environment var!"
+
+set_logFolder "${LOG_FOLDER:-${DEFAULT_LOG_FOLDER}}"
+set_logFileBaseName "${scriptBaseName}"
+
+log_message "-+*+- -+*+- -+*+- -+*+- -+*+-"
+log_message "-+*+-  Container START  -+*+-"
+log_message "-+*+- -+*+- -+*+- -+*+- -+*+-"
+
+log_message "Preparing user: ${userName}..."
+
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
+groupmod -o -g "$PGID" ${userName}
+usermod -o -u "$PUID" ${userName}
+
+log_message "Evaluating environment variables..."
+if [ -n "${TOOL_NAME}" ]; then
+  log_message "Found TOOL_NAME environment var!"
+fi
+
+# evaluate if SOURCE_FOLDER is valid, if not use default
+SOURCE_FOLDER="${SOURCE_FOLDER:-${DEFAULT_SOURCE_FOLDER}}"
+if [ ! -d "${SOURCE_FOLDER}" ]; then
+  log_message "Invalid SOURCE_FOLDER: ${SOURCE_FOLDER}"
+  SOURCE_FOLDER="${DEFAULT_SOURCE_FOLDER}"
+fi
+log_message "Using SOURCE_FOLDER: ${SOURCE_FOLDER}"
+
+# evaluate if TARGET_FOLDER is valid, if not use default
+TARGET_FOLDER="${TARGET_FOLDER:-${DEFAULT_TARGET_FOLDER}}"
+if [ ! -d "${TARGET_FOLDER}" ]; then
+  log_message "Invalid TARGET_FOLDER: ${TARGET_FOLDER}"
+  TARGET_FOLDER="${DEFAULT_TARGET_FOLDER}"
+fi
+log_message "Using TARGET_FOLDER: ${TARGET_FOLDER}"
+
+# evaluate if DATA_FOLDER is valid, if not use default
+DATA_FOLDER="${DATA_FOLDER:-${DEFAULT_DATA_FOLDER}}"
+if [ ! -d "${DATA_FOLDER}" ]; then
+  log_message "Invalid DATA_FOLDER: ${DATA_FOLDER}"
+  DATA_FOLDER="${DEFAULT_DATA_FOLDER}"
+fi
+log_message "Using DATA_FOLDER: ${DATA_FOLDER}"
+
+# evaluate if DATA_FOLDER is valid, if not use default
+ORGANIZE_CONFIG="${ORGANIZE_CONFIG:-${DEFAULT_ORGANIZE_CONFIG}}"
+if [ ! -f "${ORGANIZE_CONFIG}" ]; then
+  log_message "Invalid ORGANIZE_CONFIG: ${ORGANIZE_CONFIG}"
+  ORGANIZE_CONFIG="${DEFAULT_ORGANIZE_CONFIG}"
+fi
+log_message "Using ORGANIZE_CONFIG: ${ORGANIZE_CONFIG}"
+
+# evaluate if ORGANIZE_COMMAND is valid, if not use default
+ORGANIZE_COMMAND="${ORGANIZE_COMMAND:-${DEFAULT_ORGANIZE_COMMAND}}"
+if [ "${ORGANIZE_COMMAND}" != "sim" ] && [ "${ORGANIZE_COMMAND}" != "run" ]; then
+  log_message "Invalid ORGANIZE_COMMAND: ${ORGANIZE_COMMAND}"
+  ORGANIZE_COMMAND="${DEFAULT_ORGANIZE_COMMAND}"
+fi
+log_message "Using ORGANIZE_COMMAND: ${ORGANIZE_COMMAND}"
+
+log_message "Done evaluating environment variables."
+
+
+log_message "Preparing ${toolConfigFileName} file..."
+touch /${userHomeFolder}/${toolConfigFileName}
+
+# evaluate if TOOL_NAME was set on ENV, if so, append to conf file
+if [ -n "${TOOL_NAME}" ]; then
+  log_message "Append TOOL_NAME info to ${toolConfigFileName} file"
+  echo 'TOOL_NAME="'${TOOL_NAME}'"' >> /${userHomeFolder}/${toolConfigFileName}
+fi
+
+log_message "Append SOURCE_FOLDER info to ${toolConfigFileName} file"
+echo 'SOURCE_FOLDER="'${SOURCE_FOLDER}'"' >> /${userHomeFolder}/${toolConfigFileName}
+
+log_message "Append TARGET_FOLDER info to ${toolConfigFileName} file"
+echo 'TARGET_FOLDER="'${TARGET_FOLDER}'"' >> /${userHomeFolder}/${toolConfigFileName}
+
+log_message "Append DATA_FOLDER info to ${toolConfigFileName} file"
+echo 'DATA_FOLDER="'${DATA_FOLDER}'"' >> /${userHomeFolder}/${toolConfigFileName}
+
+log_message "Append ORGANIZE_COMMAND to ${toolConfigFileName} file"
+echo 'ORGANIZE_COMMAND="'${ORGANIZE_COMMAND}'"' >> /${userHomeFolder}/${toolConfigFileName}
+
+log_message "Append ORGANIZE_CONFIG to ${toolConfigFileName} file"
+echo 'ORGANIZE_CONFIG="'${ORGANIZE_CONFIG}'"' >> /${userHomeFolder}/${toolConfigFileName}
+
+chown ${userName}:${userName} /${userHomeFolder}/${toolConfigFileName}
+log_message "Done preparing ${toolConfigFileName} file."
+
+# check if TOOL_SCHEDULE was set on ENV. if so, set crontab schedule with it
+if [ -n "${TOOL_SCHEDULE}" ]; then
+  log_message "Found TOOL_SCHEDULE environment var!"
 
   log_message "Clear crontab schedule"
-  crontab -u ot -r 2>/dev/null | tee -a "${logFile}"
+  crontab -u ${userName} -r 2>/dev/null
+  exitCode=${?}
+  if [ ${exitCode} -gt 0 ] ; then
+    log_message "There was a problem clearing crontab schedule, exitCode was: ${exitCode}"
+  fi
 
   log_message "Set crontab schedule"
-  echo "${ORGANIZE_SCHEDULE} /home/ot/organize-run.sh" | crontab -u ot -
+  echo "${TOOL_SCHEDULE} /${userHomeFolder}/${toolScriptFileName}" | crontab -u ${userName} -
+  exitCode=${?}
+  if [ ${exitCode} -gt 0 ] ; then
+    log_message "There was a problem setting crontab schedule, exitCode was: ${exitCode}"
+  fi
 
-  log_message "restart cron service"
+  log_message "Restart cron service"
   service cron restart
   exitCode=${?}
   if [ ${exitCode} -gt 0 ] ; then
     log_message "There was a problem restarting cron service, exitCode was: ${exitCode}"
   fi
 else
-  log_message "No ORGANIZE_SCHEDULE environment var Found!"
-  log_message "This is a Single run/sim!"
-  exec /home/ot/organize-run.sh
+  log_message "No TOOL_SCHEDULE environment var Found!"
+  log_message "This is a Single run!"
+  exec sudo -u ${userName} /${userHomeFolder}/${toolScriptFileName}
   exit ${?}
 fi
 
